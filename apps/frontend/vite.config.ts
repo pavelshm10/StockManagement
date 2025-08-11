4d9b9472c64ee8cdd2b03e8cab9a533c8013be29
pavelshm10 <pavelsh00@gmail.com>
2025-08-11 14:08:05 +0300

    update

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -6,7 +6,7 @@ import { nxCopyAssetsPlugin } from '@nx/vite/plugins/nx-copy-assets.plugin';
 
 export default defineConfig(() => ({
   root: __dirname,
-  cacheDir: '../node_modules/.vite/apps',
+  cacheDir: '../node_modules/.vite/frontend',
   server: {
     port: 4200,
     host: 'localhost',
@@ -21,7 +21,7 @@ export default defineConfig(() => ({
   //  plugins: [ nxViteTsPaths() ],
   // },
   build: {
-    outDir: '../dist/apps',
+    outDir: '../dist/frontend',
     emptyOutDir: true,
     reportCompressedSize: true,
     commonjsOptions: {
